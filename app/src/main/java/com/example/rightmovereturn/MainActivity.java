f37e6fb7c2d285dc6113afc13464eb34b5c4bc65
HelloGuoYing <38399776+HelloGuoYing@users.noreply.github.com>
2018-06-04 16:20:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/rightmovereturn/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.rightmovereturn;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button)
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onViewClicked() {
+        startActivity(new Intent(MainActivity.this,Main2Activity.class));
+    }
+}
---
+++ app/src/main/java/com/example/rightmovereturn/RightReturnActivity.java
@@ -0,0 +1,97 @@
+package com.example.rightmovereturn;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class RightReturnActivity extends AppCompatActivity {
+    View decorView;
+    float downX, downY;
+    float screenWidth, screenHeight;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // setContentView(View view)方法会把view放到bgLayout中。
+        setContentView(R.layout.activity_right_return);
+
+        // 获得decorView
+        decorView = getWindow().getDecorView();
+
+        // 获得手机屏幕的宽度和高度，单位像素
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        screenWidth = metrics.widthPixels;
+        screenHeight = metrics.heightPixels;
+    }
+
+
+    /**
+     * 通过重写该方法，对触摸事件进行处理
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            downX = event.getX();
+
+        }else if(event.getAction() == MotionEvent.ACTION_MOVE){
+            float moveDistanceX = event.getX() - downX;
+            if(moveDistanceX > 0){
+                decorView.setX(moveDistanceX);
+            }
+
+        }else if(event.getAction() == MotionEvent.ACTION_UP){
+            float moveDistanceX = event.getX() - downX;
+            if(moveDistanceX > screenWidth / 2){
+                // 如果滑动的距离超过了手机屏幕的一半, 滑动处屏幕后再结束当前Activity
+                continueMove(moveDistanceX);
+            }else{
+                // 如果滑动距离没有超过一半, 往回滑动
+                rebackToLeft(moveDistanceX);
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+    /**
+     * 从当前位置一直往右滑动到消失。
+     * 这里使用了属性动画。
+     */
+    private void continueMove(float moveDistanceX){
+        // 从当前位置移动到右侧。
+        ValueAnimator anim = ValueAnimator.ofFloat(moveDistanceX, screenWidth);
+        anim.setDuration(1000); // 一秒的时间结束, 为了简单这里固定为1秒
+        anim.start();
+
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // 位移
+                float x = (float) (animation.getAnimatedValue());
+                decorView.setX(x);
+            }
+        });
+
+        // 动画结束时结束当前Activity
+        anim.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                finish();
+            }
+
+        });
+    }
+
+    /**
+     * Activity被滑动到中途时，滑回去~
+     */
+    private void rebackToLeft(float moveDistanceX){
+        ObjectAnimator.ofFloat(decorView, "X", moveDistanceX, 0).setDuration(300).start();
+    }
+}
